2059522659487fc06828827f960886bcf45aab30
Gloria <Gloria@Gloria-PC>
2023-01-03 01:38:47 +1300

    operators

---
+++ operator/src/operator/Operator.java
@@ -0,0 +1,22 @@
+package operator;
+
+public class Operator 
+{
+	public static void main(String[] args)
+	{
+	int a = 4;//arithmetic operator
+	//int b = 6 % a;//modulo operator
+	int b =9;//assignment operator
+	b += 5;
+	//System.out.println(64<65);//comparison operators
+	
+	//logical operator
+	//System.out.println(64<65 && 64<63);
+	//System.out.println(64<65 && 64<66);
+	//System.out.println(64<65|| 64<61);
+	System.out.println(2&3);//bitwise operator
+	System.out.println(b);
+
+	}
+
+}
